5ebc65ba16fdbb14e6a941d5d3f5280e79170b60
Muhammad Daffa' A.R <daffavcd@gmail.com>
2023-09-05 17:29:46 +0700

    add bg black on body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth'>
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className} style={{ backgroundColor: "black" }}>{children}</body>
     </html>
   )
 }
