1fb417d88a7962084b3cfd585e2dfa3adc5621e3
Muhammad Daffa' A.R <daffavcd@gmail.com>
2023-09-16 22:56:06 +0700

    fix failed build (unerased import)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,4 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import Header from './header'
-import Footer from './footer'
 import Content from './content'
 
 export default function About() {
